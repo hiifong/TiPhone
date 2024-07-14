64fdcb32e40bbe146ab19afebf011ecfe8e04e15
hiifong <i@hiif.ong>
2024-07-14 17:48:27 +0800

    Update

--- src/components/dock/dock.c
+++ src/components/dock/dock.c
@@ -2,7 +2,10 @@
 
 static void clicked_event_cb(lv_event_t * e)
 {
-    LV_LOG_INFO("Clicked");
+    lv_event_code_t code = lv_event_get_code(e);
+    if(code == LV_EVENT_CLICKED) {
+        LV_LOG_INFO("Clicked");
+    } 
 }
 
 lv_obj_t * t_create_dock(lv_obj_t * parent, t_dock_obj * dock)
@@ -20,9 +23,16 @@ lv_obj_t * t_create_dock(lv_obj_t * parent, t_dock_obj * dock)
     lv_style_set_bg_opa(&style, LV_OPA_20);
     lv_obj_add_style(cont_row, &style, 0);
     for(uint8_t i = 0; i < dock->len; i++) {
-        lv_obj_t * obj = lv_image_create(cont_row);
-        // lv_obj_add_style(obj, &style, 0);
-        lv_image_set_src(obj, dock->list[i].icon);
+        lv_obj_t * obj = lv_imagebutton_create(cont_row);
+        lv_obj_set_size(obj, 120, 120);
+        lv_obj_align(obj, LV_ALIGN_CENTER, 0, 0);
+        lv_imagebutton_set_src(
+            obj, 
+            LV_IMAGEBUTTON_STATE_RELEASED, 
+            dock->list[i].icon, 
+            dock->list[i].icon,
+            dock->list[i].icon
+        );
         lv_obj_add_event_cb(obj, clicked_event_cb, LV_EVENT_CLICKED, NULL);
     }
 
